bc1ee7703897cbd741992f8fbbf4d726d08ab8cd
dimarews <30553073+dimarews@users.noreply.github.com>
2021-10-21 15:28:23 +0300

    lab 5.1

---
+++ Labs/Lab5.1_Simorev.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x1, y1, x2, y2, x, y, Length;
+	cout << "Enter x1\n";
+	cin >> x1;
+	cout << "Enter y1\n";
+	cin >> y1;
+	cout << "Enter x2\n";
+	cin >> x2;
+	cout << "Enter y2\n";
+	cin >> y2;
+	x = (x1 - x2);
+	y = (y1 - y2);
+	Length = sqrt(x * x + y * y);
+	cout << "Length = " << Length << endl;
+
+	return 0;
+}
