bf51c80479cbce2bcebd8f2beb68bae9ef0c35db
dimarews <30553073+dimarews@users.noreply.github.com>
2021-10-21 15:38:18 +0300

    Add files via upload

---
+++ Labs/Lab5.2_Simorev.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double A, B, C, AC, BC, Sum;
+	cout << "Enter A\n";
+	cin >> A;
+	cout << "Enter B\n";
+	cin >> B;
+	cout << "Enter C\n";
+	cin >> C;
+	AC = abs(A - C);
+	BC = abs(B - C);
+	Sum = AC + BC;
+	cout << "AC = " << AC << endl;
+	cout << "BC = " << BC << endl;
+	cout << "Sum = " << Sum << endl;
+
+	return 0;
+}
